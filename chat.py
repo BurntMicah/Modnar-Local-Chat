8263d6d6b7c215216dc700b0355cf456c9cc46ae
BurntMicah <theledge98@hotmail.com>
2014-11-16 21:40:32 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ begip=localip.split(".")[0]+"."+localip.split(".")[1]+"."
 print "Connected to Local Network "+begip
 
 
-def server():
+def server():#Looks for inoming TCP connections and prints message along with computer
  s = socket.socket()         # Create a socket object
  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
  s.bind(("", 12233))
@@ -19,10 +19,10 @@ def server():
    # print 'Connected by', addr[0]
    data = conn.recv(1024)
    if(data):
-    print data
+    print "Computer "+addr[0]+": "+data
    conn.close()
 
-def client():
+def client():#Sends message to all ip addresses in nodes[]
  localip=([(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])     
  endip=localip.split(".")[2]+"."+localip.split(".")[3]
  while 1:
@@ -34,14 +34,14 @@ def client():
    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
    try:
     s.connect((nodes[i], 12233))
-    s.sendall("Computer "+endip+": "+message)
+    s.sendall(message)
    except:
     print nodes[i].split(".")[2]+"."+nodes[i].split(".")[3]+" has gone offline"
     nodes.remove(nodes[i])
    s.close()
 
 
-def handserver():
+def handserver():#Accepts incoming handshakes; stores ipaddress and responds with ipaddress
  UDP_IP = socket.gethostname()
  UDP_PORT = 12333
  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
@@ -54,12 +54,12 @@ def handserver():
    nodes.append(data)
    handclient(data)
 
-def handclient(value):
+def handclient(value):#Searches for computers by spitting out ipaddresss via UDP to a range of ipaddress
  localip= ([(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])           
  begip=localip.split(".")[0]+"."+localip.split(".")[1]+"."
  if(value==False):
   while 1:
-   for j in range(0,3):
+   for j in range(0,5):
     for i in range(1,255):
      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
      sock.sendto(localip, (begip+str(j)+"."+str(i), 12333))
